17f1034651ccbf79552ad529aa84d92c3fcaa885
Renato Fernandes <renatofsousa@gmail.com>
2018-11-03 03:47:54 -0300

    Added new files

---
+++ _js/script.js
@@ -0,0 +1,36 @@
+
+$(function() {
+    $('.back-to-top').hide();
+    
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn();
+        } else {
+            $('.back-to-top').fadeOut();
+        }     
+    });
+
+    $('.back-to-top').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+    });
+});
+
+$(function() {
+    $('#logo').hide();
+    
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 100) {
+            $('#logo').fadeOut();
+        } else {
+            $('#logo').fadeIn();
+        }     
+    });
+
+    $('#logo').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+    });
+});
